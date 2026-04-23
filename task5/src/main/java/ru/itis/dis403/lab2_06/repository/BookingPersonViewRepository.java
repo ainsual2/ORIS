f75faf3bc2756bf43682a3da3e7d15c7c0a980cf
karim <karimiyarullin23@gmail.com>
2026-04-23 16:51:08 +0300

    fetch(home3): переделка дз5

--- task5/src/main/java/ru/itis/dis403/lab2_06/repository/BookingPersonViewRepository.java
+++ task5/src/main/java/ru/itis/dis403/lab2_06/repository/BookingPersonViewRepository.java
@@ -7,7 +7,7 @@ import ru.itis.dis403.lab2_06.model.BookingPersonView;
 import java.util.List;
 
 @Repository
-public interface BookingPersonViewRepository extends JpaRepository<BookingPersonView, Long> {
+public interface BookingPersonViewRepository extends JpaRepository< BookingPersonView, Long> {
 
     List<BookingPersonView> findByHotelId(Long id);
 }
